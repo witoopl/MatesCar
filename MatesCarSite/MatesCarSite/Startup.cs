4ddf1d6a3c2f6effbdbf022d5a283d5ec6a3dec7
witoopl <witoopl@gmail.com>
2018-12-19 21:07:55 +0100

    Very basic login, logout and logged area

--- MatesCarSite/MatesCarSite/Startup.cs
+++ MatesCarSite/MatesCarSite/Startup.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using MatesCarSite;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,6 +28,45 @@ namespace MatesCarSite
             //Add applicationDbContext to DI
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //AddIdentity adds coockie based authentication
+            //Adds scoped classes for things like UserManger, SignInMananger, PasswordHashers etc...
+            //NOTE: Atomatically adds the validated user from a cookie to the HttpContext.User
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+
+                //Adds UserStore and RoleStore from this context
+                //That are consumed by the UserMangaer an RoleManager
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                
+                //Adds a provider that generates unique keys and hashes for things like
+                // forgot password links, phone number verification codes etc...
+                .AddDefaultTokenProviders();
+
+            //Change password policy
+            services.Configure<IdentityOptions>(options =>
+            {
+                //make really weak passwords ppossible
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+
+
+            //Alter application cookie info
+            services.ConfigureApplicationCookie(options =>
+            {
+                //Redirect to /login
+                options.LoginPath = "/login";
+                options.DataProtectionProvider = DataProtectionProvider.Create("Test");
+                //Change cookie timeout to expire in 5 days
+                options.ExpireTimeSpan = TimeSpan.FromDays(5);
+               
+            });
+            
+
+
             services.AddMvc();
         }
 
@@ -34,6 +75,9 @@ namespace MatesCarSite
         {
             //Store instance of the DI service provider so our application can access it anywhere
             IoCContainer.Provider = (ServiceProvider)serviceProvider;
+
+            //Setup identity
+            app.UseAuthentication();
 
             if (env.IsDevelopment())
             {
