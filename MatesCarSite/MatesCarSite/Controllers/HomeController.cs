85cc7e644122d6314ea7dbb391e42f8b4b335ffe
witoopl <witoopl@gmail.com>
2019-01-08 20:25:24 +0100

    Changes based on core 2 book, up to page 54ish

--- MatesCarSite/MatesCarSite/Controllers/HomeController.cs
+++ MatesCarSite/MatesCarSite/Controllers/HomeController.cs
@@ -10,28 +10,15 @@ namespace MatesCarSite.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+        public ViewResult RsvpForm()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
