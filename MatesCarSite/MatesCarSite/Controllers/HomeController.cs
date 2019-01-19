df3bee279bc775022d1590f17e115087de559d48
witoopl <witoopl@gmail.com>
2019-01-19 14:47:04 +0100

    some basic styleshhet and form. Basic communication beetween controller and view

--- MatesCarSite/MatesCarSite/Controllers/HomeController.cs
+++ MatesCarSite/MatesCarSite/Controllers/HomeController.cs
@@ -16,9 +16,29 @@ namespace MatesCarSite.Controllers
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is validation error
+                return View();
+            }
+            
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
