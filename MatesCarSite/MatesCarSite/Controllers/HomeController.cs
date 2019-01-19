1381745ee501f8d2fbdbb2929dbcae43130ff1ef
witoopl <witoopl@gmail.com>
2019-01-19 20:38:41 +0100

    Revert "Getting rid of JWT auth/ setting base for real project"
    This reverts commit 45c7b2ab2ede93067c8d1adc7614d846f3562ad4.

--- MatesCarSite/MatesCarSite/Controllers/HomeController.cs
+++ MatesCarSite/MatesCarSite/Controllers/HomeController.cs
@@ -1,157 +1,16 @@
-﻿using MatesCarSite;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using System.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MatesCarSite.Models;
 
 namespace MatesCarSite.Controllers
-{ 
-    /// <summary>
-    /// Manages the standard web server pages
-    /// </summary>
+{
     public class HomeController : Controller
     {
-        #region Protected Members
-
-        /// <summary>
-        /// The scoped Application context
-        /// </summary>
-        protected ApplicationDbContext mContexct;
-
-        /// <summary>
-        /// The manager for  handling user creation, deletion, searching, roles etc...
-        /// </summary>
-        protected UserManager<ApplicationUser> mUserManager;
-
-        /// <summary>
-        /// The manager for handling signing in and out for our users
-        /// </summary>
-        protected SignInManager<ApplicationUser> mSignInManager;
-        #endregion
-        #region Constructor
-        /// <summary>
-        /// Default constructor
-        /// </summary>
-        /// <param name="context">The injected context</param>
-        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
-        {
-            mContexct = context;
-            mUserManager = userManager;
-            mSignInManager = signInManager;
-        }
-        #endregion 
-
-        public IActionResult Index()
-        {  
-
-            mContexct.Database.EnsureCreated();
-
-            if (mContexct.Settings.Any())
-            {
-                mContexct.Settings.Add(new SettingsDataModel
-                {
-                    Name = "BackgroundColor",
-                    Value = "Red"
-                });
-                var numberOfSettings = mContexct.Settings.Local.Count();
-
-                var firstSettings = mContexct.Settings.First();
-
-                mContexct.SaveChanges();
-            }
-            
-
-
-            return View();
-        }
-        public IActionResult Error()
-        {
-            return View();
-        }
-
-        /// <summary>
-        /// Creates out single user for now
-        /// </summary>
-        /// <returns></returns>
-        [Route("create")]
-        public async Task<IActionResult> CreateUserAsync()
-        {
-            var result = await mUserManager.CreateAsync(new ApplicationUser
-            {
-                UserName = "Witold",
-                Email = "contat@me.com"
-            }, "password");
-            if(result.Succeeded)
-                return Content("User was created", "text/html");
-
-
-            return Content("User creation failed", "text/html");
-        }
-
-
-        /// <summary>
-        /// Private area no peeking.
-        /// </summary>
-        /// <returns></returns>
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [Route("private")]
-        public IActionResult Private()
-        {
-            return Content($"This is a private area. Welcome {HttpContext.User.Identity.Name}", "text/html");
-        }
-
-        [Route("logout")]
-        public async Task<IActionResult> SignOutAsync()
-        {
-            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
-            return Content("done", "text/html");
-
-        }
-
-        /// <summary>
-        /// An auto-login page for testing
-        /// </summary>
-        /// <param name="returnUrl">The url to return to if successfully logged in</param>
-        /// <returns></returns>
-        [Route("login")]
-        public async Task<IActionResult> LoginAsync(string returnUrl)
-        {
-            //Sign out any previous sessions
-            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
-
-            
-            // Sign user with the valid credentials
-            var result = await mSignInManager.PasswordSignInAsync("Witold", "password", true, false);
-            //If succesfull...
-            if (result.Succeeded)
-            {
-                //if we have no return URL
-                if (string.IsNullOrEmpty(returnUrl))
-                    //Go to home
-                    return RedirectToAction(nameof(Index));
-                //otherwise, go to the return url
-                return Redirect(returnUrl);
-            }
-
-
-            return Content("Failed to login", "text/html");
-        }
-
-        [Route("test")]
-        public SettingsDataModel Test(SettingsDataModel model)
-        {
-            return new SettingsDataModel { Id = "some id", Name = "Luke", Value = "10" };
-        }
-
-        public ViewResult NotIndex()
+        public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
@@ -175,7 +34,7 @@ namespace MatesCarSite.Controllers
                 // there is validation error
                 return View();
             }
-
+            
         }
         public ViewResult ListResponses()
         {
--- MatesCarSite/MatesCarSite/Startup.cs
+++ MatesCarSite/MatesCarSite/Startup.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using MatesCarSite;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.Formatters;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 
 namespace MatesCarSite
 {
@@ -20,75 +13,20 @@ namespace MatesCarSite
     {
         public Startup(IConfiguration configuration)
         {
-            IoCContainer.Configuration = configuration;
+            Configuration = configuration;
         }
 
-
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Add applicationDbContext to DI
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(IoCContainer.Configuration.GetConnectionString("DefaultConnection")));
-
-            //AddIdentity adds coockie based authentication
-            //Adds scoped classes for things like UserManger, SignInMananger, PasswordHashers etc...
-            //NOTE: Atomatically adds the validated user from a cookie to the HttpContext.User
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-
-                //Adds UserStore and RoleStore from this context
-                //That are consumed by the UserMangaer an RoleManager
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                
-                //Adds a provider that generates unique keys and hashes for things like
-                // forgot password links, phone number verification codes etc...
-                .AddDefaultTokenProviders();
-
-           
-            //Change password policy
-            services.Configure<IdentityOptions>(options =>
-            {
-                //make really weak passwords ppossible
-                options.Password.RequireDigit = false;
-                options.Password.RequiredLength = 5;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-            });
-
-
-            //Alter application cookie info
-            services.ConfigureApplicationCookie(options =>
-            {
-                //Redirect to /login
-                options.LoginPath = "/login";
-                //options.DataProtectionProvider = DataProtectionProvider.Create("Test");
-                //Change cookie timeout to expire in 5 days
-                options.ExpireTimeSpan = TimeSpan.FromDays(5);
-               
-            });
-            
-
-
-            services.AddMvc(options =>
-            {
-                options.InputFormatters.Add(new XmlSerializerInputFormatter());
-                options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
-
-            });
-
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //Store instance of the DI service provider so our application can access it anywhere
-            IoCContainer.Provider = (ServiceProvider)serviceProvider;
-
-            //Setup identity
-            app.UseAuthentication();
-
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -105,12 +43,7 @@ namespace MatesCarSite
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{moreInfo?}");
-
-                routes.MapRoute(
-                    name: "aboutPage",
-                    template: "more",
-                    defaults: new { controller = "About", action = "TellMeMore" });
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
