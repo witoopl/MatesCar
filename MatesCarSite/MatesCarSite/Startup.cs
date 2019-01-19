cdd746a0ee69f9f7f3ea76b5a70dd6771a1a8924
witoopl <witoopl@gmail.com>
2019-01-19 19:08:35 +0100

    Getting rid of JWT auth/ setting base for real project

--- MatesCarSite/MatesCarSite/Startup.cs
+++ MatesCarSite/MatesCarSite/Startup.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using MatesCarSite;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MatesCarSite
 {
@@ -13,20 +20,75 @@ namespace MatesCarSite
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            IoCContainer.Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //Add applicationDbContext to DI
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(IoCContainer.Configuration.GetConnectionString("DefaultConnection")));
+
+            //AddIdentity adds coockie based authentication
+            //Adds scoped classes for things like UserManger, SignInMananger, PasswordHashers etc...
+            //NOTE: Atomatically adds the validated user from a cookie to the HttpContext.User
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+
+                //Adds UserStore and RoleStore from this context
+                //That are consumed by the UserMangaer an RoleManager
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                
+                //Adds a provider that generates unique keys and hashes for things like
+                // forgot password links, phone number verification codes etc...
+                .AddDefaultTokenProviders();
+
+           
+            //Change password policy
+            services.Configure<IdentityOptions>(options =>
+            {
+                //make really weak passwords ppossible
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+
+
+            //Alter application cookie info
+            services.ConfigureApplicationCookie(options =>
+            {
+                //Redirect to /login
+                options.LoginPath = "/login";
+                //options.DataProtectionProvider = DataProtectionProvider.Create("Test");
+                //Change cookie timeout to expire in 5 days
+                options.ExpireTimeSpan = TimeSpan.FromDays(5);
+               
+            });
+            
+
+
+            services.AddMvc(options =>
+            {
+                options.InputFormatters.Add(new XmlSerializerInputFormatter());
+                options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
+            //Store instance of the DI service provider so our application can access it anywhere
+            IoCContainer.Provider = (ServiceProvider)serviceProvider;
+
+            //Setup identity
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -43,7 +105,12 @@ namespace MatesCarSite
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{moreInfo?}");
+
+                routes.MapRoute(
+                    name: "aboutPage",
+                    template: "more",
+                    defaults: new { controller = "About", action = "TellMeMore" });
             });
         }
     }
